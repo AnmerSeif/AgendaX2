1fe7a6c473da0832479a66a1cdb007330cf39093
anmerSeif <anmer92@gmail.com>
2020-07-25 17:33:29 +0200

    blog test

--- components/_Row/index.jsx
+++ components/_Row/index.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 const Row = styled.div`
+    max-width:65em;
+    margin:0 auto;
+    display: flex;
+    justify-content: space-between;
     justify-content: ${props => props.justifyContent};
     max-width:65em;
     margin:0 auto;
