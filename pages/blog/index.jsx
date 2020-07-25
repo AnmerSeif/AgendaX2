2b88ba85608386db768035b41e3cafba829dd124
anmerSeif <anmer92@gmail.com>
2020-07-25 17:30:35 +0200

    blog test

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -58,7 +58,7 @@ export default class Blog extends Component {
               {
                   postsList.map((post, index)=>{
                   return (
-                      <BlogPostCard slug={post.slug} title={post.attributes.title} excerpt={''} titleBgColor="#000000" img={post.attributes.thumbnail} category={''} date={post.date} />
+                      <BlogPostCard key={post.slug} slug={post.slug} title={post.attributes.title} excerpt={post.excerpt} titleBgColor="#000000" img={post.attributes.thumbnail} category={post.cateogry} date={post.date} />
                       )
                   })
               }
