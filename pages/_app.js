31c2a78c25c30f568de9ab727877db0344af85d6
anmerSeif <anmer92@gmail.com>
2020-08-01 18:38:42 +0200

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import breakpoints from '../breakpoints';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {fab, faFacebookF, faInstagram, faSnapchatGhost } from '@fortawesome/free-brands-svg-icons'
 library.add(fab,faFacebookF, faInstagram, faSnapchatGhost)
